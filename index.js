37ee49508a600c312c70664aed694d7cba91e0f1
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-08-30 12:33:14 -0300

    Changes

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const image_size = require('image-size')
 const pdfkit = require('pdfkit');
 const naturalCompare = require("natural-compare-lite")
 
-function dir2pdf(dir, outputPath){
-  let doc = new pdfkit({ autoFirstPage: false })
+function dir2pdf(dir, fileOutput){
+  let pdf = new pdfkit({ autoFirstPage: false })
   fs.readdir(dir, function (_,files){
     files.sort(naturalCompare).forEach( function (file){
       const filePath = `${dir}/${file}`;
         const size = image_size(filePath);
-        doc.addPage({ size: [size.width, size.height] })
-        doc.image(filePath, 0, 0, { width: size.width, height: size.height });
+        pdf.addPage({ size: [size.width, size.height] })
+        pdf.image(filePath, 0, 0, { width: size.width, height: size.height });
     });
-    doc.pipe(fs.createWriteStream(outputPath));
-    doc.end();
+    pdf.pipe(fs.createWriteStream(fileOutput));
+    pdf.end();
   })
 }
 
